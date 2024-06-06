ff1521013a1090c5f16c86203c4155022e6ef4ae
Git Kurs User <noessie@hotmail.de>
2024-06-06 09:35:50 +0200

    functions aktualisiert

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,32 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const FirebaseConfig = require("./FirebaseConfig");
+const functions = FirebaseConfig.functions;
+const firestore = FirebaseConfig.firestore;
+const storageBucket = FirebaseConfig.storageBucket;
+const auth = FirebaseConfig.auth;
+const admin = FirebaseConfig.admin;
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+exports.onCreateRecipe = functions.firestore
+  .document("recipes/{recipeId}")
+  .onCreate(async (snapshot) => {
+    const countDocRef = firestore.collection("recipesCounts").doc("all");
+    const countDoc = await countDocRef.get();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+    if (countDoc.exists) {
+      countDocRef.update({ count: admin.firestore.FieldValue.increment(1) });
+    } else {
+      countDocRef.set({ count: 1 });
+    }
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    const recipe = snapshot.data();
+
+    if (recipe.isPublished) {
+      const countPublishedDocRef = firestore.collection("recipesCounts").doc("published");
+      const countPublishedDoc = await countPublishedDocRef.get();
+
+      if (countPublishedDoc.exists) {
+        countPublishedDocRef.update({ count: admin.firestore.FieldValue.increment(1) });
+      } else {
+        countPublishedDocRef.set({ count: 1 });
+      }
+    }
+  });
